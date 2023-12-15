b19dd6dc79bec3ac33c09d6fa7c6f67af17f6d5e
Supriya Roy <supriyaroy366@gmail.com>
2023-12-15 14:53:34 +0530

    Update 2. ArrayRotation.java

--- 2. ArrayRotation.java
+++ 2. ArrayRotation.java
@@ -1,4 +1,7 @@
-/* Results: https://app.codility.com/demo/results/training3U822J-4X3/
+/* 
+Results: https://app.codility.com/demo/results/training3U822J-4X3/
+Problem: https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+
 An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
 
 The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
