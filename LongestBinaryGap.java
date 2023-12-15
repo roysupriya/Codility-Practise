b36ec6a349af0ff1d733126c7865caf38fa26690
Supriya Roy <supriyaroy366@gmail.com>
2023-12-15 13:54:53 +0530

    Update LongestBinaryGap.java

--- LongestBinaryGap.java
+++ LongestBinaryGap.java
@@ -1,4 +1,4 @@
-/* 
+/* Problem 1:
 Results: https://app.codility.com/demo/results/training97MT3U-45T/
 
 A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
