dd6b48b3dce7c25d520de55577c6151dcad40bb4
Supriya Roy <supriyaroy366@gmail.com>
2023-12-15 11:21:09 +0530

    Create MissingIntegerInArray.java
    Write a function:
    
    class Solution { public int solution(int[] A); }
    that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
    For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
    Given A = [1, 2, 3], the function should return 4.
    Given A = [−1, −3], the function should return 1.
    Write an efficient algorithm for the following assumptions:
    
    N is an integer within the range [1..100,000];
    each element of array A is an integer within the range [−1,000,000..1,000,000].

---
+++ MissingIntegerInArray.java
@@ -0,0 +1,39 @@
+/* Write a function:
+
+class Solution { public int solution(int[] A); }
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+Given A = [1, 2, 3], the function should return 4.
+Given A = [−1, −3], the function should return 1.
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
+
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // Implement your solution here
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int item : A) {
+            set.add(item);
+        }
+
+        for(int i=1; i<= A.length + 1; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+
+        return 1;
+    }
+}
+
