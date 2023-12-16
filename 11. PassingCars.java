21fd6f7b089853b903dd8c71327b06a1501a19df
Supriya Roy <supriyaroy366@gmail.com>
2023-12-17 00:18:09 +0530

    Update and rename 11. PassingCars to 11. PassingCars.java

--- 11. PassingCars.java
+++ 11. PassingCars.java
@@ -1,8 +1,10 @@
 /*
 Problem: https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
-Results:
+Results: https://app.codility.com/demo/results/trainingPW5ZK3-NPH/
 Theory: https://codility.com/media/train/3-PrefixSums.pdf
 Others: https://www.geeksforgeeks.org/prefix-sum-array-implementation-applications-competitive-programming/
+
+Time: O(N), Space: O(1)
 
 
 A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
@@ -43,3 +45,44 @@ Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [1..100,000];
 each element of array A is an integer that can have one of the following values: 0, 1.
+*/
+
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        
+        /*
+            Keep a counter for Cars moving east,
+            Whenever encountering west passing cars, 
+                -> Increase passing car count by number of east passing cars to take care of multiple combinations
+        */
+
+        int eastCars = 0;
+        int passingCars = 0;
+
+        for(int i=0; i<A.length ; i++){
+            if(A[i] == 0) {
+                eastCars++;
+            }
+            else if (A[i] == 1) {
+                passingCars = passingCars + eastCars;
+            }
+        }
+
+        /* Note:
+            -> Can use _ for big values in place of commas
+            -> check for -ve value in case of overflows
+        */
+
+        if (passingCars > 1_000_000_000 || passingCars < 0){
+            return -1;
+        } else {
+            return passingCars;
+        }
+    }
+}
